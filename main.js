014305e98f39974b9e108d0bb6da6ba434817d1f
khadija <59174800+khadija-nur@users.noreply.github.com>
2020-06-09 14:37:30 +0100

    Display picked options
    when options are picked, the screen will display options picked by both players
    A play again button will also be displayed

--- main.js
+++ main.js
@@ -20,6 +20,51 @@ optionsContainer.addEventListener("click", e =>{
    const userAnswerValue = e.target.classList.value;
    mainBody.style.display = "none";
 
+      if (e.target == machineAnswer){
+         results.appendChild(e.target);
+         houseResults.appendChild(machineAnswer);
+         console.log((e.target).classList.value);
+         console.log("you draw")
+      }
+      else if(machineAnswerValue == "rock" ){
+         if(userAnswerValue == "paper"){
+            results.appendChild(e.target);
+         houseResults.appendChild(machineAnswer);
+            resultscomment.innerHTML = "you win paper covers rock";
+
+         }
+         else{
+            results.appendChild(e.target);
+            results.appendChild(machineAnswer);
+            resultscomment.innerHTML = "rock crushes scissors";
+         }
+      }
+      else if(machineAnswerValue == "paper"){
+         if(userAnswerValue == "rock"){
+            resultscomment.innerHTML = "house wins paper covers rock";
+            results.appendChild(e.target);
+            results.appendChild(machineAnswer);
+         }
+         else{
+            results.appendChild(e.target);
+            results.appendChild(machineAnswer);
+            resultscomment.innerHTML = "scissors cut paper";
+         }
+      }
+      else{
+         if(userAnswerValue == "rock"){
+            results.appendChild(e.target);
+            results.appendChild(machineAnswer);
+            resultscomment.innerHTML = "you win rock crushes scissors";
+
+         }
+         else{
+            results.appendChild(e.target);
+            results.appendChild(machineAnswer);
+            resultscomment.innerHTML = "you lose scissors cut paper";
+
+         }
+      }
    }
 })
 

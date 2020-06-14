71ff79fa9a44949e4909d1ab4efcaeb5a9363542
khadija <59174800+khadija-nur@users.noreply.github.com>
2020-06-14 06:58:59 +0100

    display results icons

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Define variables
 const optionsContainer =document.querySelector(".icons");
+
 const options = Array.from(optionsContainer.children);
-const machineAnswer = options[Math.floor(Math.random() * 3)];
 const userScore = document.querySelector(".scoreCount");
 scoreCount = 0;
 
@@ -45,49 +45,58 @@ optionsContainer.addEventListener("click", e =>{
       // console.log(resultscomment.innerHTML);
 
       if (userAnswerValue == machineAnswerValue){
+         resultscomment.textContent = "you draw";
+
+         // console.log("you draw")
+         showResults(machineAnswerValue, userAnswerValue);
       }
       else if(machineAnswerValue == "rock" ){
          if(userAnswerValue == "paper"){
-            userResults.appendChild(e.target);
-            houseResults.appendChild(machineAnswer);
-            resultscomment.innerHTML = "you win";
+            resultscomment.textContent = "you win";
 
+            showResults("rock","paper");
          }
          else{
-            userResults.appendChild(e.target);
-            houseResults.appendChild(machineAnswer);
-            resultscomment.innerHTML = "you lose";
+            resultscomment.textContent = "you lose";
+
+            showResults("rock","scissors");
          }
       }
       else if(machineAnswerValue == "paper"){
          if(userAnswerValue == "rock"){
-            resultscomment.innerHTML = "house wins";
-            userResults.appendChild(e.target);
-            houseResults.appendChild(machineAnswer);
+            resultscomment.textContent = "House wins";
+
+            showResults("paper","rock");
          }
          else{
-            userResults.appendChild(e.target);
-            houseResults.appendChild(machineAnswer);
-            resultscomment.innerHTML = "you win";
+            resultscomment.textContent = "You win";
+
+            showResults("paper","scissors");
          }
       }
       else{
          if(userAnswerValue == "rock"){
-            userResults.appendChild(e.target);
-            houseResults.appendChild(machineAnswer);
-            resultscomment.innerHTML = "you win";
-
+            resultscomment.textContent = "you win";
+            showResults("scissors","rock");
          }
          else{
-            userResults.appendChild(e.target);
-            houseResults.appendChild(machineAnswer);
-            resultscomment.innerHTML = "you lose";
+            resultscomment.textContent = "you lose";
 
+            showResults("scissors","paper");
          }
-      }
+      } 
+      console.log(resultscomment.textContent);
+ 
       
-
-
+      if (resultscomment.textContent == "you lose"){
+         console.log(scoreCount)
+         scoreCount += -1 ;
+         userScore.innerHTML = String(scoreCount);
+      }
+      else if (resultscomment.textContent == "you win"){
+         scoreCount += 1 ;
+         userScore.innerHTML = String(scoreCount);
+      }
 })
 
 playBtn.addEventListener("click", e =>{

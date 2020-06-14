c938aa26cab1ff150202b6965ef61187402cca53
khadija <59174800+khadija-nur@users.noreply.github.com>
2020-06-14 10:56:05 +0100

    fix typography

--- main.js
+++ main.js
@@ -48,42 +48,42 @@ optionsContainer.addEventListener("click", e =>{
       // console.log(resultscomment.innerHTML);
 
       if (userAnswerValue == machineAnswerValue){
-         resultscomment.textContent = "you draw";
+         resultscomment.textContent = "you draw!";
 
          // console.log("you draw")
          showResults(machineAnswerValue, userAnswerValue);
       }
       else if(machineAnswerValue == "rock" ){
          if(userAnswerValue == "paper"){
-            resultscomment.textContent = "you win";
+            resultscomment.textContent = "you win!";
 
             showResults("rock","paper");
          }
          else{
-            resultscomment.textContent = "you lose";
+            resultscomment.textContent = "you lose!";
 
             showResults("rock","scissors");
          }
       }
       else if(machineAnswerValue == "paper"){
          if(userAnswerValue == "rock"){
-            resultscomment.textContent = "House wins";
+            resultscomment.textContent = "House wins!";
 
             showResults("paper","rock");
          }
          else{
-            resultscomment.textContent = "You win";
+            resultscomment.textContent = "You win!";
 
             showResults("paper","scissors");
          }
       }
       else{
          if(userAnswerValue == "rock"){
-            resultscomment.textContent = "you win";
+            resultscomment.textContent = "you win!";
             showResults("scissors","rock");
          }
          else{
-            resultscomment.textContent = "you lose";
+            resultscomment.textContent = "you lose!";
 
             showResults("scissors","paper");
          }
@@ -91,12 +91,12 @@ optionsContainer.addEventListener("click", e =>{
       console.log(resultscomment.textContent);
  
       
-      if (resultscomment.textContent == "you lose"){
+      if (resultscomment.textContent == "you lose!"){
          console.log(scoreCount)
          scoreCount += -1 ;
          userScore.innerHTML = String(scoreCount);
       }
-      else if (resultscomment.textContent == "you win"){
+      else if (resultscomment.textContent == "you win!"){
          scoreCount += 1 ;
          userScore.innerHTML = String(scoreCount);
       }
